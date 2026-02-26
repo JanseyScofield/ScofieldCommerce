82bd491544b62dd7bf149f9c47cbf9c30fd95e27
JanseyScofield <janscofield10@gmail.com>
2026-02-25 23:09:46 -0300

    🔥Remove(Domain): Removendo lógica de associação de preço de cada produto para cada cliente

--- src/API/ScofieldCommerce.Domain/Exceptions/PrecoClienteProdutoException.cs
+++
@@ -1,7 +0,0 @@
-namespace ScofieldCommerce.Domain.Exceptions
-{
-    public class PrecoClienteProdutoException : Exception
-    {
-        public PrecoClienteProdutoException(string message) : base(message) { }
-    }
-}
